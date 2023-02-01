7cb4a7bdc63a4cfe0c373a72082d0a9feb0e4bc4
Roshan Shrestha <30362960@students.federation.edu.au>
2023-02-01 15:51:46 +1100

    Final

--- pages/index.js
+++ pages/index.js
@@ -15,38 +15,6 @@ export default function Home() {
           <SearchBox placeholder='Search for a city...' />
           <FamousPlaces />
         </div>
-        <hr /> 
-        <div>
-          <div className='row justify-content-center'>
-          <div className="card-body">
-          <div className='row'>
-            <form className='user'>
-              <div className='form-group'>
-                <input type="email" className='form-control form-control-user' placeholder='Enter Email Address...' />
-              </div>
-              <div className='form-group'>
-                <input type="password" className='form-control form-control-user' placeholder='Password' />
-              </div>
-              <Link href='' legacyBehavior>
-                <a className='btn btn-primary btn-user btn-block'>Login</a>
-              </Link>
-              <hr />
-              <div className='text-center'>
-                <Link href='' legacyBehavior>
-                  <a className='small'>Forgot Password?</a>
-                </Link>
-              </div>
-              <div className='text-center'>
-                <Link href='' legacyBehavior>
-                  <a className='small'>Create an Account!</a>
-                </Link>
-              </div>
-              
-            </form>
-            </div>
-          </div>
-          </div>
-          </div>
 
         </div>
       
